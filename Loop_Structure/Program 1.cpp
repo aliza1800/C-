4c30f4fe7b0bd27836fce5f7b6f81bf463601d0f
aliza1800 <alizaiqbal018@gmail.com>
2025-02-06 11:11:10 +0500

    ..

--- Loop_Structure/Program 1.cpp
+++ Loop_Structure/Program 1.cpp
@@ -1,1 +1,12 @@
+//	Write a program to print numbers from 1 to 10 using a for loop.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    for(x=0;x<=10;x++){
+    	cout<<x<<endl;
+	}
+	return 0;
+}
 
--- Loop_Structure/Program9.cpp
+++ Loop_Structure/Program9.cpp
@@ -1,1 +1,16 @@
+//	Write a program to display the sum of even numbers between 1 and 50 using a for loop.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=0;
+    for(int i=2;i<=50;i+=2){
+    	cout<<i<<endl;
+    	sum+=i;
+	}
+	cout<<"Total sum of number is"<<sum<<endl;
+	
+}
+	return 0;
+}
 
